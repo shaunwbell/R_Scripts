6af7fa055ab60ca3bf2402758cc99698b4c0163c
shaunwbell <bell@updraft.pmel.noaa.gov>
2016-09-14 07:13:37 -0700

    update to add alim

--- ArcticHeat/AXBT_profile_plot.R
+++ ArcticHeat/AXBT_profile_plot.R
@@ -23,7 +23,7 @@ png(filename=save_file,
 ### Using ggplot2
 p <- ggplot(axbt.data, aes(x=X.C.,y=Depth))
 p <- p + geom_point(color='red', cex=0.25)
-p <- p + scale_y_continuous(trans='reverse')
+p <- p + scale_y_continuous(trans='reverse', limits=c(NaN,0))
 p <- p + ggtitle(basename(myfile))
 p
 
